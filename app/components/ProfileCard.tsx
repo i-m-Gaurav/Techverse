341311fdb2a934517666ba4f399a7f3b8e753f62
Gaurav Kumar <indiagauravkumar@gmail.com>
2024-07-14 08:53:38 +0530

    chore: Update ProfileCard component styling and structure

--- app/components/ProfileCard.tsx
+++ app/components/ProfileCard.tsx
@@ -8,14 +8,13 @@ interface ProfileCardProps {
     _id: string;
     name: string;
     profession: string;
-    about: string;
     image: string;
   }
 }
 
 const ProfileCard: React.FC<ProfileCardProps> = ({ user }) => {
   return (
-    <div className="bg-gray-900 p-8 rounded-3xl max-w-xs mx-auto h-[500px] flex flex-col justify-between">
+    <div className="bg-[#121e2c] shadow-md p-8 rounded-xl w-full max-w-[300px] min-w-[280px] mx-auto h-[400px] flex flex-col justify-between items-center">
       <div className="flex flex-col items-center">
         <div className="w-32 h-32 rounded-full overflow-hidden mb-6">
           <Image
@@ -26,17 +25,22 @@ const ProfileCard: React.FC<ProfileCardProps> = ({ user }) => {
             height={128}
           />
         </div>
-        <h2 className="text-white text-2xl font-bold mb-2">{user.name}</h2>
-        <p className="text-gray-400 text-sm mb-6">{user.profession}</p>
-        <p className="text-gray-300 text-sm text-center mb-8">
-          {user.about}
-        </p>
+       <div className='flex flex-col justify-center items-center'>
+       <h2 className="text-white text-2xl font-bold mb-2">{user.name}</h2>
+        <p className="text-gray-400 text-xs mb-6">{user.profession}</p>
+       </div>
+        
       </div>
-      <Link href={`/profile/${user._id}`} passHref>
-        <button className="bg-blue-500 text-white py-3 px-6 rounded-full text-sm font-semibold hover:bg-blue-600 transition duration-300 w-full">
-          View Profile
-        </button>
-      </Link>
+      <div className="mt-auto">
+        <Link href={`/ProfilePage/${user._id}`} passHref>
+          <button className="px-8 py-2 rounded-full relative bg-transparent text-white text-sm   transition duration-200 border border-slate-600">
+            <div className="absolute inset-x-0 h-px w-1/2 mx-auto -top-px shadow-2xl bg-gradient-to-r from-transparent via-teal-500 to-transparent" />
+            <span className="relative z-20">
+              View Profile
+            </span>
+          </button>
+        </Link>
+      </div>
     </div>
   );
 };
