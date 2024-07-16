37f0bfd2735b7cdc2111949e8c162e68e8790c69
Gaurav Kumar <indiagauravkumar@gmail.com>
2024-07-16 22:03:38 +0530

    refactor: Update Home component to include session as a dependency for fetching users

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
     if (status === "authenticated") {
       fetchUsers();
     }
-  }, [status]);
+  }, [status,session]);
 
   if (status === "loading" || loading) {
     return (
